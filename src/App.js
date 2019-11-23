96769446eaaa2aed8b05adc06352b163df64d9f9
Low Jun Chen Marx <marxlow94@gmail.com>
2019-11-23 19:45:31 +0800

    Introduce react-router with authLayout skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-// Layouts
+// Layouts & Route
+import routes from "./routes";
 import PublicLayout from "./shared/layout/PublicLayout";
-// import AuthLayout from './shared/layout/AuthLayout';
+import AuthLayout from "./shared/layout/AuthLayout";
 
 // Public pages
 import LoginPage from "./pages/LoginPage";
 
 // Authenticated pages
+import DashboardPage from "./pages/DashboardPage";
+
+const pages = [
+  // Public pages
+  {
+    exact: true,
+    path: routes.login,
+    component: LoginPage,
+    layout: PublicLayout
+  },
+  // Auth pages
+  {
+    exact: false,
+    path: routes.dashboard,
+    component: DashboardPage,
+    layout: AuthLayout
+  }
+];
 
 const App = () => {
+  const history = createBrowserHistory();
+
   return (
-    <PublicLayout>
-      <LoginPage />
-    </PublicLayout>
+    <Router history={history}>
+      <Switch>
+        {pages.map(
+          ({ exact, path, component: Component, layout: Layout }, index) => (
+            <Route
+              key={index}
+              exact={exact}
+              path={path}
+              component={props => (
+                <Layout>
+                  <Component {...props} />
+                </Layout>
+              )}
+            />
+          )
+        )}
+        <Redirect to={routes.dashboard} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/DashboardPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <span>Hello world!</span>
+      <span>Hello world!</span>
+    </div>
+  );
+};
+
+export default DashboardPage;
